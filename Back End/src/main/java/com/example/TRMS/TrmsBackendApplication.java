69d2d65fd253e1999468ffcba347ebe5d9d8887a
Rivanman <94255757+rivanarsdel@users.noreply.github.com>
2022-09-20 09:41:36 -0400

    File Structure Discrepancy

---
+++ Back End/src/main/java/com/example/TRMS/TrmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TRMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrmsBackendApplication.class, args);
+	}
+
+}
---
+++ Back End/src/test/java/com/example/TRMS/TrmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TRMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
