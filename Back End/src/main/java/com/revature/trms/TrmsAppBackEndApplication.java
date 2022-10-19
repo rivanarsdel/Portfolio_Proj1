2c313aacceebbe7b957f40ff90c748e541b296a3
Rivanman <94255757+rivanarsdel@users.noreply.github.com>
2022-10-19 17:52:32 -0400

    Re initialized back end as spring project

---
+++ Back End/src/main/java/com/revature/trms/TrmsAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.revature.trms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrmsAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrmsAppBackEndApplication.class, args);
+	}
+
+}
---
+++ Back End/src/test/java/com/revature/trms/TrmsAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.trms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrmsAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
